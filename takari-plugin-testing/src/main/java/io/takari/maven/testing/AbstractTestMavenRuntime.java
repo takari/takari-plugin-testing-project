e79bf08a60cbe21afd3774e14392068cd8de7a39
Philippe Marschall <philippe.marschall@gmail.com>
2021-12-26 23:36:17 +0100

    Extract abstract base classes for code reuse

---
+++ takari-plugin-testing/src/main/java/io/takari/maven/testing/AbstractTestMavenRuntime.java
@@ -0,0 +1,284 @@
+/**
+ * Copyright (c) 2014 Takari, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package io.takari.maven.testing;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecution;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.DefaultPlexusContainer;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+
+import com.google.inject.Module;
+
+public abstract class AbstractTestMavenRuntime {
+
+  private static final DefaultArtifactVersion MAVEN_VERSION;
+
+  static {
+    DefaultArtifactVersion version = null;
+    String path = "/META-INF/maven/org.apache.maven/maven-core/pom.properties";
+    try (InputStream is = TestMavenRuntime.class.getResourceAsStream(path)) {
+      Properties properties = new Properties();
+      if (is != null) {
+        properties.load(is);
+      }
+      String property = properties.getProperty("version");
+      if (property != null) {
+        version = new DefaultArtifactVersion(property);
+      }
+    } catch (IOException e) {
+      // odd, where did this come from
+    }
+    MAVEN_VERSION = version;
+  }
+
+  private static interface RuntimeFactory {
+    MavenRuntime newInstance(Module[] modules) throws Exception;
+  }
+
+  // ordered map of supported maven runtime factories
+  private static final Map<VersionRange, RuntimeFactory> FACTORIES;
+
+  static {
+    Map<VersionRange, RuntimeFactory> factories = new LinkedHashMap<>();
+    try {
+      factories.put(VersionRange.createFromVersionSpec("[3.0,3.1.1)"), new RuntimeFactory() {
+        @Override
+        public MavenRuntime newInstance(Module[] modules) throws Exception {
+          return new Maven30xRuntime(modules);
+        }
+      });
+      factories.put(VersionRange.createFromVersionSpec("[3.1.1,3.2.1)"), new RuntimeFactory() {
+        @Override
+        public MavenRuntime newInstance(Module[] modules) throws Exception {
+          return new Maven311Runtime(modules);
+        }
+      });
+      factories.put(VersionRange.createFromVersionSpec("[3.2.1,3.2.5)"), new RuntimeFactory() {
+        @Override
+        public MavenRuntime newInstance(Module[] modules) throws Exception {
+          return Maven321Runtime.create(modules);
+        }
+      });
+      factories.put(VersionRange.createFromVersionSpec("[3.2.5]"), new RuntimeFactory() {
+        @Override
+        public MavenRuntime newInstance(Module[] modules) throws Exception {
+          return new Maven325Runtime(modules);
+        }
+      });
+      // the last entry is expected to handle everything else
+      factories.put(VersionRange.createFromVersionSpec("(3.2.5,]"), new RuntimeFactory() {
+        @Override
+        public MavenRuntime newInstance(Module[] modules) throws Exception {
+          return new Maven331Runtime(modules);
+        }
+      });
+    } catch (InvalidVersionSpecificationException e) {
+      throw new RuntimeException(e);
+    }
+    FACTORIES = Collections.unmodifiableMap(factories);
+  }
+
+  private final Module[] modules;
+  private MavenRuntime runtime;
+
+  public class TestDependency {
+
+    private final File file;
+    private String groupId = "test";
+    private String artifactId;
+    private String classifier;
+    private String version = "1.0";
+    private String type = "jar";
+    private String scope = Artifact.SCOPE_COMPILE;
+    private boolean optional;
+
+    private TestDependency(File artifact) {
+      this.file = artifact;
+      this.artifactId = artifact.getName();
+    }
+
+    public TestDependency setArtifactId(String artifactId) {
+      this.artifactId = artifactId;
+
+      return this;
+    }
+
+    public TestDependency setGroupId(String groupId) {
+      this.groupId = groupId;
+
+      return this;
+    }
+
+    public TestDependency setVersion(String version) {
+      this.version = version;
+
+      return this;
+    }
+
+    public TestDependency setType(String type) {
+      this.type = type;
+
+      return this;
+    }
+
+    public TestDependency setOptional(boolean optional) {
+      this.optional = optional;
+
+      return this;
+    }
+
+    public TestDependency setClassifier(String classifier) {
+      this.classifier = classifier;
+
+      return this;
+    }
+
+    public TestDependency setScope(String scope) {
+      this.scope = scope;
+
+      return this;
+    }
+
+    public TestDependency addTo(MavenProject project) throws Exception {
+      return addTo(project, true);
+    }
+
+    public TestDependency addTo(MavenProject project, boolean direct) throws Exception {
+      ArtifactHandler handler = getContainer().lookup(ArtifactHandler.class, type);
+      DefaultArtifact artifact = new DefaultArtifact(groupId, artifactId, version, scope, type, classifier, handler);
+      artifact.setFile(file);
+      artifact.setOptional(optional);
+      Set<Artifact> artifacts = project.getArtifacts();
+      artifacts.add(artifact);
+      project.setArtifacts(artifacts);
+      if (direct) {
+        Set<Artifact> directDependencies = project.getDependencyArtifacts();
+        directDependencies = directDependencies == null ? new LinkedHashSet<Artifact>() : new LinkedHashSet<>(directDependencies);
+        directDependencies.add(artifact);
+        project.setDependencyArtifacts(directDependencies);
+      }
+
+      return this;
+    }
+  }
+
+  AbstractTestMavenRuntime() {
+    this(new Module[0]);
+  }
+
+  AbstractTestMavenRuntime(Module... modules) {
+    this.modules = modules;
+  }
+
+  void createMavenRuntime() throws Exception {
+    runtime = newMavenRuntime(modules);
+  }
+
+  void shutDownMavenRuntime() {
+    runtime.shutdown();
+    runtime = null;
+  }
+
+  private MavenRuntime newMavenRuntime(Module[] modules) throws Exception {
+    for (Map.Entry<VersionRange, RuntimeFactory> entry : FACTORIES.entrySet()) {
+      if (entry.getKey().containsVersion(MAVEN_VERSION)) {
+        return entry.getValue().newInstance(modules);
+      }
+    }
+    throw new AssertionError(String.format("Maven version %s is not supported, supprted versions: %s", MAVEN_VERSION, FACTORIES.entrySet()));
+  }
+
+  public MavenProject readMavenProject(File basedir) throws Exception {
+    MavenProject project = runtime.readMavenProject(basedir);
+    Objects.requireNonNull(project);
+    return project;
+  }
+
+  public MavenSession newMavenSession(MavenProject project) throws Exception {
+    MavenSession session = runtime.newMavenSession(project.getBasedir());
+    session.setCurrentProject(project);
+    session.setProjects(Arrays.asList(project));
+    return session;
+  }
+
+  /** @since 2.9 */
+  public MojoExecution newMojoExecution(String goal, Xpp3Dom... parameters) {
+    MojoExecution execution = runtime.newMojoExecution(goal);
+    if (parameters != null) {
+      // TODO decide if this should go to runtime.newMojoExecution
+      Xpp3Dom configuration = execution.getConfiguration();
+      for (Xpp3Dom parameter : parameters) {
+        configuration.addChild(parameter);
+      }
+    }
+    return execution;
+  }
+
+  public void executeMojo(File basedir, String goal, Xpp3Dom... parameters) throws Exception {
+    MavenProject project = readMavenProject(basedir);
+    MavenSession session = newMavenSession(project);
+    executeMojo(session, project, goal, parameters);
+  }
+
+  public void executeMojo(MavenSession session, MavenProject project, String goal, Xpp3Dom... parameters) throws Exception {
+    MojoExecution execution = newMojoExecution(goal, parameters);
+    executeMojo(session, project, execution);
+  }
+
+  public void executeMojo(MavenProject project, String goal, Xpp3Dom... parameters) throws Exception {
+    MavenSession session = newMavenSession(project);
+    executeMojo(session, project, goal, parameters);
+  }
+
+  public void executeMojo(MavenSession session, MavenProject project, MojoExecution execution) throws Exception {
+    runtime.executeMojo(session, project, execution);
+  }
+
+  public Mojo lookupConfiguredMojo(MavenSession session, MojoExecution execution) throws Exception {
+    return runtime.lookupConfiguredMojo(session, execution);
+  }
+
+  public DefaultPlexusContainer getContainer() {
+    return runtime.getContainer();
+  }
+
+  public <T> T lookup(Class<T> role) throws Exception {
+    return runtime.lookup(role);
+  }
+
+  public static Xpp3Dom newParameter(String name, String value) {
+    Xpp3Dom child = new Xpp3Dom(name);
+    child.setValue(value);
+    return child;
+  }
+
+  public TestDependency newDependency(File artifact) {
+    return new TestDependency(artifact);
+  }
+
+}
--- takari-plugin-testing/src/main/java/io/takari/maven/testing/TestResources.java
+++ takari-plugin-testing/src/main/java/io/takari/maven/testing/TestResources.java
@@ -7,269 +7,38 @@
  */
 package io.takari.maven.testing;
 
-import java.io.BufferedReader;
-
-// some of the code was originally copied from org.apache.maven.plugin.testing.resources.TestResources
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
-import java.util.TreeSet;
-
-import org.codehaus.plexus.util.DirectoryScanner;
-import org.codehaus.plexus.util.FileUtils;
-import org.junit.Assert;
 import org.junit.Rule;
-import org.junit.rules.TestWatcher;
+import org.junit.rules.TestRule;
 import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 
 /**
  * Junit4 test {@link Rule} to extract and assert test resources.
  */
-public class TestResources extends TestWatcher {
-
-  private final String projectsDir;
-
-  private final String workDir;
-
-  private String name;
+public class TestResources extends AbstractTestResources implements TestRule {
 
   public TestResources() {
-    this("src/test/projects", "target/test-projects");
+    super();
   }
 
   public TestResources(String projectsDir, String workDir) {
-    this.projectsDir = projectsDir;
-    this.workDir = workDir;
+    super(projectsDir, workDir);
   }
 
   @Override
-  protected void starting(Description d) {
-    String methodName = d.getMethodName();
-    if (methodName != null) {
-      methodName = methodName.replace('/', '_').replace('\\', '_');
-    }
-    name = d.getTestClass().getSimpleName() + "_" + methodName;
-  }
-
-  /**
-   * Creates new clean copy of test project directory structure. The copy is named after both the test being executed and test project name, which allows the same test project can be used by multiple
-   * tests and by different instances of the same parametrized tests.<br/>
-   * TODO Provide alternative working directory naming for Windows, which still limits path names to ~250 charecters
-   */
-  public File getBasedir(String project) throws IOException {
-    if (name == null) {
-      throw new IllegalStateException(getClass().getSimpleName() + " must be a test class field annotated with org.junit.Rule");
-    }
-    File basedir = new File(workDir, name + "_" + project).getCanonicalFile();
-    FileUtils.deleteDirectory(basedir);
-    Assert.assertTrue("Test project working directory created", basedir.mkdirs());
-    File src = new File(projectsDir, project).getCanonicalFile();
-    Assert.assertTrue("Test project directory does not exist: " + src.getPath(), src.isDirectory());
-    FileUtils.copyDirectoryStructure(src, basedir);
-    return basedir;
-  }
-
-  /**
-   * Creates new clean test work directory. The directory is named after test being executed.
-   * 
-   * @since 2.2
-   */
-  public File getBasedir() throws IOException {
-    if (name == null) {
-      throw new IllegalStateException(getClass().getSimpleName() + " must be a test class field annotated with org.junit.Rule");
-    }
-    File basedir = new File(workDir, name).getCanonicalFile();
-    FileUtils.deleteDirectory(basedir);
-    Assert.assertTrue("Test project working directory created", basedir.mkdirs());
-    return basedir;
-  }
-
-  // static helpers
-
-  public static void cp(File basedir, String from, String to) throws IOException {
-    // TODO ensure destination lastModified timestamp changes
-    FileUtils.copyFile(new File(basedir, from), new File(basedir, to));
-  }
-
-  public static void assertFileContents(File basedir, String expectedPath, String actualPath) throws IOException {
-    String expected = fileRead(new File(basedir, expectedPath), true);
-    String actual = fileRead(new File(basedir, actualPath), true);
-    Assert.assertEquals(expected, actual);
-  }
-
-  private static String fileRead(File file, boolean normalizeEOL) throws IOException {
-    StringBuilder sb = new StringBuilder();
-    try (BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
-      if (normalizeEOL) {
-        String str;
-        while ((str = r.readLine()) != null) {
-          sb.append(str).append('\n');
-        }
-      } else {
-        int ch;
-        while ((ch = r.read()) != -1) {
-          sb.append((char) ch);
-        }
+  public Statement apply(Statement base, Description d) {
+    return new Statement() {
+      @Override
+      public void evaluate() throws Throwable {
+        starting(d.getTestClass(), d.getMethodName());
+        base.evaluate();
       }
-    }
-    return sb.toString();
+    };
   }
 
-  public static void assertFileContents(String expectedContents, File basedir, String path) throws IOException {
-    String actualContents = fileRead(new File(basedir, path), true);
-    Assert.assertEquals(expectedContents, actualContents);
+  @Override
+  String getRequiredAnnotationClassName() {
+    return "org.junit.Rule";
   }
 
-  public static void assertDirectoryContents(File dir, String... expectedPaths) {
-    DirectoryScanner scanner = new DirectoryScanner();
-    scanner.setBasedir(dir);
-    scanner.addDefaultExcludes();
-    scanner.scan();
-
-    Set<String> actual = new TreeSet<String>();
-    for (String path : scanner.getIncludedFiles()) {
-      actual.add(path.replace(File.separatorChar, '/'));
-    }
-    for (String path : scanner.getIncludedDirectories()) {
-      if (path.length() > 0) {
-        actual.add(path.replace(File.separatorChar, '/') + "/");
-      }
-    }
-
-    Set<String> expected = new TreeSet<String>();
-    if (expectedPaths != null) {
-      for (String path : expectedPaths) {
-        expected.add(path.replace(File.separatorChar, '/'));
-      }
-    }
-
-    // compare textual representation to make diff easier to understand
-    Assert.assertEquals(toString(expected), toString(actual));
-  }
-
-  private static String toString(Collection<String> strings) {
-    StringBuilder sb = new StringBuilder();
-    for (String string : strings) {
-      sb.append(string).append('\n');
-    }
-    return sb.toString();
-  }
-
-  public static void touch(File basedir, String path) throws InterruptedException {
-    touch(new File(basedir, path));
-  }
-
-  public static void touch(File file) throws InterruptedException {
-    if (!file.isFile()) {
-      throw new IllegalArgumentException("Not a file " + file);
-    }
-    long lastModified = file.lastModified();
-    file.setLastModified(System.currentTimeMillis());
-
-    // TODO do modern filesystems still have this silly lastModified resolution?
-    if (lastModified == file.lastModified()) {
-      Thread.sleep(1000L);
-      file.setLastModified(System.currentTimeMillis());
-    }
-  }
-
-  public static void rm(File basedir, String path) {
-    Assert.assertTrue("delete " + path, new File(basedir, path).delete());
-  }
-
-  public static void create(File basedir, String... paths) throws IOException {
-    if (paths == null || paths.length == 0) {
-      throw new IllegalArgumentException();
-    }
-    for (String path : paths) {
-      File file = new File(basedir, path);
-      file.getParentFile().mkdirs();
-      Assert.assertTrue(file.getParentFile().isDirectory());
-      file.createNewFile();
-      Assert.assertTrue(file.isFile() && file.canRead());
-    }
-  }
-
-  public static void assertFilesPresent(File basedir, String... paths) {
-    if (basedir == null || paths == null || paths.length <= 0) {
-      throw new IllegalArgumentException();
-    }
-    if (paths.length == 1) {
-      Assert.assertTrue(paths[0] + " PRESENT", new File(basedir, paths[0]).isFile());
-    } else {
-      StringBuilder expected = new StringBuilder();
-      StringBuilder actual = new StringBuilder();
-      for (String path : paths) {
-        expected.append(path).append("\n");
-        if (!new File(basedir, path).isFile()) {
-          actual.append("NOT PRESENT ");
-        }
-        actual.append(path).append("\n");
-      }
-      Assert.assertEquals(expected.toString(), actual.toString());
-    }
-  }
-
-  public static void assertFilesNotPresent(File basedir, String... paths) {
-    if (basedir == null || paths == null || paths.length <= 0) {
-      throw new IllegalArgumentException();
-    }
-    if (paths.length == 1) {
-      Assert.assertFalse(paths[0] + " NOT PRESENT", new File(basedir, paths[0]).isFile());
-    } else {
-      StringBuilder expected = new StringBuilder();
-      StringBuilder actual = new StringBuilder();
-      for (String path : paths) {
-        expected.append(path).append("\n");
-        if (new File(basedir, path).isFile()) {
-          actual.append("PRESENT ");
-        }
-        actual.append(path).append("\n");
-      }
-      Assert.assertEquals(expected.toString(), actual.toString());
-    }
-  }
-
-  /**
-   * @since 2.2
-   */
-  public static Map<String, String> readProperties(File basedir, String path) throws IOException {
-    Properties properties = new Properties();
-    try (InputStream is = new FileInputStream(new File(basedir, path))) {
-      properties.load(is);
-    }
-    Map<String, String> result = new HashMap<>();
-    for (String key : properties.stringPropertyNames()) {
-      result.put(key, properties.getProperty(key));
-    }
-    return Collections.unmodifiableMap(result);
-  }
 }
